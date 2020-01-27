7dafe3748c91716e37c923bc42794b6a0c0612f4
Mamun746 <abdullahalmamun914@gmail.com>
2020-01-27 10:45:04 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import Recipe from "./components/Recipe";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    search: "",
+    recipes: []
+  };
+ 
+  handleOnChange = (e) => {
+    this.setState({
+      search: e.target.value
+    });
+  };
+
+  getUserQuery = (e) => {
+    e.preventDefault();
+    const API_ID = "08e859db";
+    const API_KEY = "11be6e3cb741906244ce57bf6fdc94f8";
+    axios
+      .get(
+        `https://api.edamam.com/search?q=${this.state.search}&app_id=${API_ID}&app_key=${API_KEY}`
+      )
+      .then((res) => {
+        this.setState({
+          recipes: res.data.hits
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    this.setState({
+      search: ""
+    });
+  };
+  componentDidMount() {
+    const json = localStorage.getItem("recipes");
+    const recipes = JSON.parse(json);
+    this.setState({
+      recipes
+    });
+  }
+  componentDidUpdate() {
+    const recipes = JSON.stringify(this.state.recipes);
+    localStorage.setItem("recipes", recipes);
+  }
+
+
+  render() {
+    return (
+      <div className='App'>
+        <form className='search' onSubmit={this.getUserQuery}>
+          <input
+            className='search-bar'
+            type='text'
+            value={this.state.search}
+            onChange={this.handleOnChange}
+          />
+          <button className='search-button' type='submit'>
+            Submit
+          </button>
+        </form>
+        <Recipe />
+        {this.state.recipes.length ? (
+          <div>
+            {this.state.recipes.map((recipe) => (
+              <Recipe
+                key={recipe.recipe.label + recipe.recipe.calories}
+                title={recipe.recipe.label}
+                calories={recipe.recipe.calories}
+                image={recipe.recipe.image}
+              />
+            ))}
+          </div>
+        ) : (
+          <div>Loading..</div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
